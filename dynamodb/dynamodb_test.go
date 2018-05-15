710946bcbc061354ed48494068224dbb1deb1fea
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2018-05-15 22:15:57 +0200

    Always log error

--- dynamodb/dynamodb_test.go
+++ dynamodb/dynamodb_test.go
@@ -62,9 +62,7 @@ func TestDynamoDBFeatureStore_UseLdd(t *testing.T) {
 	// Since we're using an invalid SDK key, this should return the default value
 	ldUser := ld.NewUser("dynamo-store-test")
 	flag, err := ldClient.IntVariation("some.unknown.feature", ldUser, 2018)
-	if err != nil {
-		t.Log(err)
-	}
+	t.Log(err)
 	if flag != 2018 {
 		t.Fatal("flag not set to default value")
 	}
