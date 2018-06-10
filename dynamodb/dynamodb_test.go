af6f22ea90bf818f2a4e8259dfc66d63005ff434
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2018-06-10 15:00:20 +0200

    Simplify test setup

--- dynamodb/dynamodb_test.go
+++ dynamodb/dynamodb_test.go
@@ -12,27 +12,17 @@ import (
 
 const envTable = "LAUNCHDARKLY_DYNAMODB_TABLE"
 
-// StoreBuilder allows us to access the testing context from within the
-// function passed to RunFeatureStoreTests.
-type StoreBuilder struct {
-	t     *testing.T
-	table string
-}
-
-func (builder *StoreBuilder) Build() ld.FeatureStore {
-	store, err := dynamodb.NewDynamoDBFeatureStore(builder.table, nil)
-	if err != nil {
-		builder.t.Fatal(err)
-	}
-	return store
-}
-
 func TestDynamoDBFeatureStore(t *testing.T) {
 	table := os.Getenv(envTable)
 	if table == "" {
 		t.Skipf("%s not set in environment", envTable)
 	}
 
-	builder := StoreBuilder{t, table}
-	ldtest.RunFeatureStoreTests(t, builder.Build)
+	ldtest.RunFeatureStoreTests(t, func() ld.FeatureStore {
+		store, err := dynamodb.NewDynamoDBFeatureStore(table, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		return store
+	})
 }
